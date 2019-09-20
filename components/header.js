cc90daa20b903f6c05f49fea3caa1efe2174ff4a
Faith Pueneh <puenehfaith@gmail.com>
2019-09-20 11:25:17 +0100

    updated

--- components/header.js
+++ components/header.js
@@ -4,6 +4,19 @@ import Menu from '../components/Menu';
 const Header = () => (
 <div className="header-wrapper">
     <Menu/>
+    <style jsx>{`
+    .header-wrapper{
+        padding:20px;
+        display:flex;
+    }
+        justify-content:space-between;
+        @media (max-width: 360px) {
+            .header-wrapper {
+              display: block;
+              text-align: center;
+            
+    }
+    `}</style>
 </div>
 );
 export default Header;
