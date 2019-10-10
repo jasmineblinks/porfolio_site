af639dcd4cb0f7b785de4d4c880d06ebe9a56c51
Faith Pueneh <puenehfaith@gmail.com>
2019-10-10 23:56:18 +0100

    updated

--- pages/blog.js
+++ pages/blog.js
@@ -1,10 +1,32 @@
 
 import Layout from '../components/layout';
 const Blog = () => (
-    <div className="container">
-        <Layout/>
-      <h1>Blog</h1>
-    </div>
+  <Layout title="About">
+        
+      <h1>Blog page</h1>
+
+      <style jsx>{`
+    *{
+      margin : 0px;
+       padding: 0px;
+    }
+     .about-head{
+       text-align: center;
+      margin:auto;
+       font-family:BlinkMacSystemFont;
+      background: linear-gradient(
+        rgba(232, 104, 93, 0.45),
+        rgba(232, 104, 93, 0.45) 
+      
+      ), url("/static/lap.jpg");
+      height:100px;
+      color:#fff;
+      
+
+     }
+     `}</style>
+    </Layout>
+    
   );
   
   export default Blog;
