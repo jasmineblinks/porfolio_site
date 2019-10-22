a6272d5eab8147bbeb6375b7d099a5d90af4e75f
Faith Pueneh <puenehfaith@gmail.com>
2019-10-22 21:26:31 +0100

    updated

--- components/footer.js
+++ components/footer.js
@@ -8,9 +8,7 @@ const Footer = () => (
         color:#fff;
         margin-top: 80px;
         padding: 25px 20px;
-        position: absolute;
-        bottom: 0;
-         width: 100%;
+        
         
       }
       .copyright {
